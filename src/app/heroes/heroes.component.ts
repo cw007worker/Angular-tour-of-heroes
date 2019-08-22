d61fc1f4790aa13bc0f15f7a8229587b5d24fe08
linwinpei <52321057+linwinpei@users.noreply.github.com>
2019-08-23 00:49:07 +0800

    add list and master/detail components

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from './../mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +9,14 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
   hero: Hero = {
-    id: 1,
-    name: "William"
+    name: "william",
+    id: 1
+  }
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
   constructor() { }
 
